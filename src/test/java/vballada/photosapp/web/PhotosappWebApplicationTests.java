4a1dc472454ba0eb2d29f6acd0275098a2b12836
ballada_v <vballada@hotmail.com>
2018-04-10 10:07:42 +0200

    Faster Unit Tests thanks to @DataMongoTest

--- src/test/java/vballada/photosapp/web/PhotosappWebApplicationTests.java
+++ src/test/java/vballada/photosapp/web/PhotosappWebApplicationTests.java
@@ -2,11 +2,13 @@ package vballada.photosapp.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataMongoTest
 public class PhotosappWebApplicationTests {
 
 	@Test
