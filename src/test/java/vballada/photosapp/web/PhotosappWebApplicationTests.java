1f6b595eadee070ed5c48726a3bbdda33b5ac0df
ballada_v <vballada@hotmail.com>
2018-04-10 14:22:40 +0200

    Adding embedded MongoDB for testing

--- src/test/java/vballada/photosapp/web/PhotosappWebApplicationTests.java
+++ src/test/java/vballada/photosapp/web/PhotosappWebApplicationTests.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @DataMongoTest
+@ActiveProfiles("embedded")
 public class PhotosappWebApplicationTests {
 
 	@Test
