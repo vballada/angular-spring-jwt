8d990d8456f9e8eb19bc29dbd1bc02e8066a3099
ballada_v <vballada@hotmail.com>
2018-04-24 11:44:24 +0200

    Adding more relevant data

--- src/main/java/vballada/photosapp/web/PhotosappWebApplication.java
+++ src/main/java/vballada/photosapp/web/PhotosappWebApplication.java
@@ -1,6 +1,7 @@
 package vballada.photosapp.web;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
@@ -24,6 +25,12 @@ import vballada.photosapp.web.domain.PhotoRepository;
 public class PhotosappWebApplication {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(PhotosappWebApplication.class);
+	
+	private static final String [] CITIES = {"Paris", "Vannes", "Amiens", "Caen", "Rouen"};
+	
+	private static final String [] USERS = {"vballada", "a_user", "b_user", "c_user", "d_user"};
+	
+	private Random generator = new Random();
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotosappWebApplication.class, args);
@@ -42,15 +49,16 @@ public class PhotosappWebApplication {
 		try {
 			repository.deleteAll();
 			return args -> {
-				Stream.of("photo_1.jpg", "photo_2.jpg", "photo_3.jpg", "photo_4.jpg").forEach(name -> {
-					Photo photo = new Photo();
-					photo.setId(Integer.valueOf(name.substring(6, 7)));
-					photo.setDatetime(LocalDateTime.now());
-					photo.setUsername("vballada");
-					photo.setLocation("Paris");
-					photo.setFilename(name);
-					repository.save(photo);
-				});
+				Stream.of("photo_1.jpg", "photo_2.jpg", "photo_3.jpg", "photo_4.jpg", "photo_5.jpg", "photo_6.jpg",
+						"photo_7.jpg", "photo_8.jpg", "photo_9.jpg", "photo_10.jpg", "photo_11.jpg").forEach(name -> {
+							Photo photo = new Photo();
+							photo.setId(Integer.valueOf(name.substring(6, 7)));
+							photo.setDatetime(LocalDateTime.now());
+							photo.setUsername(USERS[generator.nextInt(USERS.length)]);
+							photo.setLocation(CITIES[generator.nextInt(CITIES.length)]);
+							photo.setFilename(name);
+							repository.save(photo);
+						});
 				repository.findAll().forEach(System.out::println);
 			};
 		} catch (DataAccessResourceFailureException e) {
