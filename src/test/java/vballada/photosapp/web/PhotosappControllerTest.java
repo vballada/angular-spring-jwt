d315ffe0403aa7a329cef3a34bfa05e1021411c3
ballada_v <vballada@hotmail.com>
2018-04-26 17:27:41 +0200

    Fixing unit test

--- src/test/java/vballada/photosapp/web/PhotosappControllerTest.java
+++ src/test/java/vballada/photosapp/web/PhotosappControllerTest.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,6 +28,7 @@ import vballada.photosapp.web.domain.Photo;
 import vballada.photosapp.web.domain.PhotoRepository;
 
 @RunWith(SpringRunner.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(controllers = PhotosappController.class, secure = false)
 public class PhotosappControllerTest {
 
