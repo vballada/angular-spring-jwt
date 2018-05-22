a0efb204316fcc19a74d1dc84939b0f3d1475f18
ballada_v <vballada@hotmail.com>
2018-05-22 10:29:27 +0200

    Adding the photo slider

--- script/photosapp/src/app/detailphotos/detailphotos.component.ts
+++ script/photosapp/src/app/detailphotos/detailphotos.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-detailphotos',
-  templateUrl: './detailphotos.component.html',
-  styleUrls: ['./detailphotos.component.css']
+    selector: 'app-detailphotos',
+    templateUrl: './detailphotos.component.html',
+    styleUrls: ['./detailphotos.component.css']
 })
 export class DetailphotosComponent implements OnInit {
 
-  constructor() { }
+    images = ['http://localhost:8080/assets/img/photo_1.jpg', 'http://localhost:8080/assets/img/photo_2.jpg', 'http://localhost:8080/assets/img/photo_3.jpg'];
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
