781e880c32222a3df0676bd94ed6f48905bbaf7e
ballada_v <vballada@hotmail.com>
2018-05-24 18:32:19 +0200

    Login page

--- script/photosapp/src/app/app-routing.module.ts
+++ script/photosapp/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PhotosComponent }      from './photos/photos.component';
 import { DetailphotosComponent }      from './detailphotos/detailphotos.component';
+import { LoginComponent }      from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: PhotosComponent },
   { path: 'lstphotos', component: PhotosComponent },
-  { path: 'detailphotos', component: DetailphotosComponent }
+  { path: 'detailphotos', component: DetailphotosComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
